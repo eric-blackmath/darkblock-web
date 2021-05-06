3d674bcddcbbf73c4f171b825bab378a6c400e3a
Eric Flatt <eric@blackmath.com>
2021-05-06 17:08:21 -0400

    updating

--- src/controller/file.controller.js
+++ src/controller/file.controller.js
@@ -1,6 +1,14 @@
 const uploadFile = require("../middleware/upload");
 const fs = require("fs");
 const baseUrl = "http://localhost:8080/files/";
+import Arweave from "arweave";
+import { Component } from "react";
+
+const arweave = Arweave.init({
+  host: "arweave.net",
+  port: 1984,
+  protocol: "http",
+});
 
 const upload = async (req, res) => {
   try {
@@ -9,21 +17,26 @@ const upload = async (req, res) => {
     if (req.file == undefined) {
       return res.status(400).send({ message: "Please upload a file!" });
     }
+    // put arweave stuff then delete itself, same directory path
+    let walletFile = fs.readFileSync(
+      "C:/Users/eflat/Documents/arweave-wallet/arweave-key-JxRiV4nzg46XiKVZrvVbirHI3VWKjtAbIGPoBgKSD4w"
+    ); //to wallet file
+    wallet = JSON.parse(walletFile);
+    key = await arweave.wallets.jwkToAddress(wallet);
+
+    let data = fs.readFileSync(
+      "C:/Users/eflat/darkblock-react/resources/static/assets/uploads/cursor.png"
+    ); //this is being uploaded
+    let transaction = await arweave.createTransaction({ data: data }, key);
+    transaction.addTag("Content-Type", "image/jpeg");
+    // transaction.addTag('Content-Type', 'application/pdf');
+    await arweave.transactions.sign(transaction, key);
+
+    const response = await arweave.transactions.post(transaction);
+    console.log(response.status);
 
     res.status(200).send({
       message: "Uploaded the file successfully: " + req.file.originalname,
-      // put arweave stuff then delete itself, same directory path
-      // let walletFile = fs.readFileSync('C:/Users/eflat/Documents/arweave-wallet/arweave-key-JxRiV4nzg46XiKVZrvVbirHI3VWKjtAbIGPoBgKSD4w');//to wallet file
-      // wallet = JSON.parse(walletFile);
-      // key = await arweave.wallets.jwkToAddress(wallet);
-
-      // let data = fs.readFileSync('path/to/file.pdf');//this is being uploaded
-      // let transaction = await arweave.createTransaction({ data: data }, key);
-      // // transaction.addTag('Content-Type', 'application/pdf');
-      // await arweave.transactions.sign(transaction, key);
-
-      // const response = await arweave.transactions.post(transaction);
-      // console.log(response.status);
     });
   } catch (err) {
     res.status(500).send({
@@ -34,7 +47,6 @@ const upload = async (req, res) => {
 
 const getListFiles = (req, res) => {
   const directoryPath = __basedir + "/resources/static/assets/uploads/";
-
 
   fs.readdir(directoryPath, function (err, files) {
     if (err) {
