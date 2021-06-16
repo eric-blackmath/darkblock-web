2abdd48ca45c95308acdc1fbf0c202b1beb5d42a
eric-blackmath <eric@blackmath.com>
2021-06-16 16:21:11 -0400

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, {Component} from "react";
-// import MetaMaskLoginButton from 'react-metamask-login-button';
+import React, { Component } from "react";
 
 import Meta from "../src/components/meta";
-import Users from "../src/components/rarible";
-import Image from "../src/components/image";
 import Axios from "../src/components/axios";
-// import Form from "../src/components/form";
-// import ImagePreview from "../src/components/imagePreview";
 
-// import Contacts from './components/contacts'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-      <Meta />
-      {/* <ImagePreview /> */} 
-      <Axios />
-      {/* <Image /> */}
-      {/* <Form /> */}
-    </div>
-      // <Contacts contacts={this.state.contacts} />
-    )
+        <Meta />
+        <Axios />
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
