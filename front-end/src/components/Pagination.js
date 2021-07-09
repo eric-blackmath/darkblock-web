b8d683d780f408d3a50b4877b2610a7e790997f5
eric-blackmath <eric@blackmath.com>
2021-07-09 15:31:20 -0400

    pagination

--- front-end/src/components/Pagination.js
+++ front-end/src/components/Pagination.js
@@ -1,46 +1,46 @@
-import React from "react";
+// import React from "react";
 
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
-  const pageNumbers = [];
+// const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+//   const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNumbers.push(i);
-  }
-
-  return (
-    <nav>
-      <ul className="pagination">
-        {pageNumbers.map((number) => (
-          <li key={number} className="page-item">
-            <button onClick={() => paginate(number)}>{number}</button>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-};
-
-export default Pagination;
-
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Pagination from '@material-ui/lab/Pagination';
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     '& > *': {
-//       marginTop: theme.spacing(2),
-//     },
-//   },
-// }));
-
-// export default function PaginationRounded() {
-//   const classes = useStyles();
+//   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+//     pageNumbers.push(i);
+//   }
 
 //   return (
-//     <div className={classes.root}>
-//       <Pagination count={10} variant="outlined" shape="rounded" />
-//     </div>
+//     <nav>
+//       <ul className="pagination">
+//         {pageNumbers.map((number) => (
+//           <li key={number} className="page-item">
+//             <button onClick={() => paginate(number)}>{number}</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </nav>
 //   );
-// }
+// };
+
+// export default Pagination;
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Pagination from '@material-ui/lab/Pagination';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+export default function PaginationRounded() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Pagination count={10} variant="outlined" shape="rounded" />
+    </div>
+  );
+}
 
