86749482d181d8f9e3265aa0f2a40d246b07c7c9
eric-blackmath <eric@blackmath.com>
2021-07-12 09:16:37 -0400

    styling

--- front-end/src/components/MyNfts.js
+++ front-end/src/components/MyNfts.js
@@ -87,6 +87,7 @@ export default function MyNfts() {
 
   return (
     <React.Fragment>
+      
       {/* <button>Go to detailsView</button> */}
       {isLoaded ? (
         <ul className="list-group">
