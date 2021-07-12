e8595eb435c1bc2add83beb0b67a00c4eec8037f
Kurtz00420 <Merccury2019>
2021-07-12 17:23:01 +0500

    -login/logout -redirect on login -no nfts found logic -pagination logic for new ui -tv login ui logic -should we keep the user logged in after tvLogin? @Darri -create darkblock button disabled when isAlreadyDarkblocked or isUploading -did some testing with 50+ accounts from opensea, handled a bunch of errors, specially for createdbyme request, too much inconsistency in data (some of the assets are collections), still needs to be tested thoroughly. do we have time to write some end-to-end tests for a couple of components? @Chris -added a test route for createdbyme, /nfts/createdbyme/:account -node endpoint (/verify-sig) added for signature verification, post request, takes {signature, address, data} in form-data, data:String (full string of the original data, then formatted on server-side) -tags changes

--- front-end/__tests__/MyNftsTest.js
+++ front-end/__tests__/MyNftsTest.js
@@ -1,22 +1,3 @@
 import * as React from "react";
 import { Router } from "react-router";
 import { App } from "../src/App";
-
-import { rtlRender, screen, userEvent } from "@testing-library/react";
-
-// pretend this is in another file, and we:
-// import {LocationDisplay} from './location-display'
-const render = (ui, { route = "/nfts/all" } = {}) => {
-  window.history.pushState({}, "Test page", route);
-
-  return rtlRender(ui, { wrapper: Router });
-};
-
-test("full app rendering/navigating", () => {
-  render(<App />);
-  //   expect(screen.getByText(/you are home/i)).toBeInTheDocument();
-
-  userEvent.click(screen.getByText(/about/i));
-
-  expect(screen.getByText(/you are on the about page/i)).toBeInTheDocument();
-});
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,7 +8,6 @@ import DetailsView from "./components/Details";
 import Nav from "./components/Nav";
 import { UserContext } from "./util/UserContext";
 import { Redirect } from "react-router";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const [address, setAddress] = useState();
@@ -27,54 +26,52 @@ function App() {
       {address ? <Nav setAddress={setAddress} /> : null}
 
       <div className="content">
-        <BrowserRouter>
-          <Switch>
-            <UserContext.Provider value={address}>
-              <Route
-                exact
-                path="/nfts/all"
-                render={() => (address ? <MyNfts /> : <Redirect to="/home" />)}
-              ></Route>
+        <Switch>
+          <UserContext.Provider value={address}>
+            <Route
+              exact
+              path="/nfts/all"
+              render={() => (address ? <MyNfts /> : <Redirect to="/home" />)}
+            ></Route>
 
-              <Route
-                exact
-                path="/nfts/all/:account"
-                render={() => (address ? <MyNfts /> : <Redirect to="/home" />)}
-              ></Route>
+            <Route
+              exact
+              path="/nfts/all/:account"
+              render={() => (address ? <MyNfts /> : <Redirect to="/home" />)}
+            ></Route>
 
-              <Route exact path="/home">
-                <Home setAddress={setAddress} />
-              </Route>
+            <Route exact path="/home">
+              <Home setAddress={setAddress} />
+            </Route>
 
-              <Route
-                exact
-                path="/nfts/createdbyme"
-                render={() =>
-                  address ? <CreatedByMe /> : <Redirect to="/home" />
-                }
-              ></Route>
+            <Route
+              exact
+              path="/nfts/createdbyme"
+              render={() =>
+                address ? <CreatedByMe /> : <Redirect to="/home" />
+              }
+            ></Route>
 
-              <Route
-                exact
-                path="/nfts/createdbyme:account"
-                render={() =>
-                  address ? <CreatedByMe /> : <Redirect to="/home" />
-                }
-              ></Route>
+            <Route
+              exact
+              path="/nfts/createdbyme/:account"
+              render={() =>
+                address ? <CreatedByMe /> : <Redirect to="/home" />
+              }
+            ></Route>
 
-              <Route
-                path="/details/:contract/:token"
-                render={() =>
-                  address ? <DetailsView /> : <Redirect to="/home" />
-                }
-              ></Route>
+            <Route
+              path="/details/:contract/:token"
+              render={() =>
+                address ? <DetailsView /> : <Redirect to="/home" />
+              }
+            ></Route>
 
-              <Route exact path="/tv">
-                <Tv />
-              </Route>
-            </UserContext.Provider>
-          </Switch>
-        </BrowserRouter>
+            <Route exact path="/tv">
+              <Tv />
+            </Route>
+          </UserContext.Provider>
+        </Switch>
       </div>
     </div>
   );
