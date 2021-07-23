7f93d431120bd0db3db6e44793b4f515ee08809e
eric-blackmath <eric@blackmath.com>
2021-07-23 10:00:07 -0400

    footer reset

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -8,13 +8,11 @@ import hands from "../images/hands.png";
 import goldblock from "../images/goldblock.png";
 import silverblock from "../images/silverblock.png";
 import tvimage from "../images/tvimage.png";
-import FooterHome from "../components/footerhome";
+import Footer from "../components/footer";
 
 //Logs user into metamask and fetches their account address
 export default function Home({ setAddress }) {
-  const fhome = {
-    position: 'relative !important'
-  }
+
   let history = useHistory();
 
   const getAccount = async () => {
@@ -120,7 +118,7 @@ export default function Home({ setAddress }) {
           </div>
         </div>
       </div>
-      <FooterHome />
+      <Footer />
     </div>
   );
 }
