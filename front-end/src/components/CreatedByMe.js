1e5c36312e2d641e46ba03aa6138205763a7f3cb
eric-blackmath <eric@blackmath.com>
2021-07-28 10:56:06 -0400

    footer fix

--- front-end/src/components/CreatedByMe.js
+++ front-end/src/components/CreatedByMe.js
@@ -66,7 +66,7 @@ export default function CreatedByMe() {
     <React.Fragment>
       {/* <button>Go to detailsView</button> */}
       {isLoaded ? (
-        <div className="list-height">
+        <div>
           <ul className="list-group">
             {currentNftsMeta.map((listitem, index) => (
               <NFTItem key={index} nft={nfts[nfts.indexOf(listitem)]} />
